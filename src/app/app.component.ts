6b04311e9389096f994c2e933aadf0355fa412f3
IR <na@intelligentreach.com>
2022-08-06 11:15:30 +0100

    Amended debounceTime/switchMap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
-import { debounceTime, distinctUntilChanged, take} from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, take, switchMap} from 'rxjs/operators';
 import { Product } from './models/product';
 import { ProductsService } from './products/products.service';
 
@@ -13,6 +14,7 @@ export class AppComponent implements OnInit {
   title = 'ir-dev-test';
 
   searchString = '';
+  searchControl: FormControl = new FormControl();
   brands: string[] = [];
   products: Product[] = [];
   products$: Observable<Product[]>;
@@ -20,14 +22,12 @@ export class AppComponent implements OnInit {
   constructor(private productService: ProductsService) {}
   ngOnInit(): void {
     this.products$ = this.productService.getProducts();
-  }
-  onSearchStringChanged(inputChanged: Observable<string>): void{
-    inputChanged.pipe(
-      debounceTime(400),
+    this.searchControl.valueChanges
+    .pipe(
+      debounceTime(1000),
       take(4),
       distinctUntilChanged(),
-    ).subscribe( _ => this.products$ = this.productService.getProducts());
-
+      switchMap(() => this.productService.getProducts())
+    );
   }
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductsModule } from './products/products.module';
@@ -12,7 +12,7 @@ import { ProductsModule } from './products/products.module';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    ReactiveFormsModule,
     ProductsModule
   ],
   providers: [],
